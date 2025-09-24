4cdff07d1838747bd2aa96304fcb9ec1219f8b45
eli <eliadadearaujolinhares@gmail.com>
2025-09-24 20:41:35 -0300

    Add files via upload

---
+++ atividade08.py
@@ -0,0 +1,15 @@
+# Atividade 08
+
+# Peça ao usuário:
+# Nome
+# Ano de nascimento
+# Calcule sua idade aproximada e mostre a mensagem:
+# ___, você tem ___ anos.
+
+
+nome = str(input("digite o seu nome: "))
+ano_de_nascimento = int(input("digite o ano do seu nascimento: "))
+idade = 2025 - ano_de_nascimento 
+print(f'{nome}, você tem {idade} anos.')
+
+
